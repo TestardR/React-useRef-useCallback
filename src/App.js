10b1bb29d30aafb51b427a8a1f52e96aa64914ce
unknown <rmtestard@gmail.com>
2020-03-29 13:42:44 +0200

    I have finalized the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 
 function App() {
+  const renders = useRef(0);
+  console.log('hello renders: ', renders.current++);
+  // const inputRef = useRef(null);
+  const [inputRef, setInputRef] = useState(null);
+  const [count, setCount] = useState(0);
+  const counterRef = useRef(0);
+
+  const setRef = useCallback(node => {
+    setInputRef(node);
+  }, []);
+
+  // const setRef = node => {
+  //   setInputRef(node)
+  // }
+
+  useEffect(() => {
+    if (!inputRef) {
+      return;
+    }
+
+    function focusInput() {
+      console.log('I have focused the input with DOM value :', inputRef);
+    }
+
+    inputRef.addEventListener('focus', focusInput);
+    inputRef.focus();
+
+    return () => {
+      inputRef.removeEventListener('focus', focusInput);
+    };
+  }, [inputRef]);
+
+  function incrementCounterRef() {
+    counterRef.current += 1;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>State count: {count}</p>
+      <input ref={setRef} placeholder="Click me Banana" />
+      <button
+        type="button"
+        onClick={() => setCount(prevCount => prevCount + 1)}
+      >
+        Increment
+      </button>
+
+      <p>Ref Count: {counterRef.current}</p>
+      <button type="button" onClick={incrementCounterRef}>
+        Increment Ref Count
+      </button>
     </div>
   );
 }
